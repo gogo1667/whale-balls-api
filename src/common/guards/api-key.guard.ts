9c3601e63b17d7bac1d0f4d1169e87c7bd184258
gogo1667 <gogo1667@gmail.com>
2026-02-17 09:16:31 -0600

    api-key

---
+++ src/common/guards/api-key.guard.ts
@@ -0,0 +1,25 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class ApiKeyGuard implements CanActivate {
+  constructor(private configService: ConfigService) {}
+
+  canActivate(context: ExecutionContext): boolean {
+    const request = context.switchToHttp().getRequest();
+    const apiKey = request.headers['x-api-key'];
+
+    const validKey = this.configService.get<string>('WHALE_BALLS_API_KEY');
+
+    if (!apiKey || apiKey !== validKey) {
+      throw new UnauthorizedException('Invalid API key');
+    }
+
+    return true;
+  }
+}
--- src/scores/scores.controller.ts
+++ src/scores/scores.controller.ts
@@ -1,6 +1,8 @@
-import { Controller, Get, Post, Body } from '@nestjs/common';
+import { Controller, Get, Post, Body, UseGuards } from '@nestjs/common';
 import { ScoresService } from './scores.service';
+import { ApiKeyGuard } from '../common/guards/api-key.guard';
 
+@UseGuards(ApiKeyGuard)
 @Controller('scores')
 export class ScoresController {
   constructor(private readonly scoresService: ScoresService) {}
